f4dc887f27deaa5f853137ea339aa0de72e311d6
HoBo <1781194173@qq.com>
2021-01-10 20:58:04 +0800

    1.10  投资列表

--- service-core/src/main/java/com/atguigu/srb/core/controller/LendController.java
+++ service-core/src/main/java/com/atguigu/srb/core/controller/LendController.java
@@ -1,9 +1,20 @@
 package com.atguigu.srb.core.controller;
 
 
+import com.atguigu.common.result.R;
+import com.atguigu.srb.core.pojo.entity.Lend;
+import com.atguigu.srb.core.service.LendService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +24,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Helen
  * @since 2020-12-12
  */
+@Api(tags = "标的")
 @RestController
-@RequestMapping("/lend")
+@RequestMapping("/api/core/lend")
+@CrossOrigin
+@Slf4j
 public class LendController {
 
+    @Autowired
+    LendService lendService;
+
+    @ApiOperation("获取标的列表")
+    @GetMapping("/list")
+    public R list(){
+        List<Lend> lendList = lendService.selectList();
+        return R.ok().data("lendList", lendList);
+    }
 }
 
